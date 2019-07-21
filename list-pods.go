48dbbff529f78ecc439410a7540540ce560ba627
Aly Ragab <AlyRagab70@gmail.com>
2019-07-21 04:04:57 +0200

    Update list-pods.go

--- list-pods.go
+++ list-pods.go
@@ -10,26 +10,27 @@ import (
         "flag"
         "path/filepath"
 )
-
+var err error
+func check(err error){
+        if err != nil {
+                log.Fatal(err)
+        }   
+     }
 func main() {
-
         // Access to the Cluster using the kubeconfig file 
-
         kubeconfig := flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube","config"),"/home/aly/.kube/config")
         flag.Parse()
+        
         config, err :=clientcmd.BuildConfigFromFlags("", *kubeconfig)
-                if err != nil {
-                        log.Fatal(err)
-                }
+        check(err)
+        
         clientset, err := kubernetes.NewForConfig(config)
-                if err != nil {
-                        log.Fatal(err)
-                }
+        check(err)
+        
         // List the available pods in the cluster using K8s API
         pods , err := clientset.CoreV1().Pods("") .List(metav1.ListOptions{})
-                if err != nil {
-                        panic(err.Error())
-                }
+        check(err)
+        
         // Loop to all Pods and print them out 
         fmt.Printf("There are %d Pods available in the Cluster \n", len(pods.Items))
         for _, pod := range pods.Items {
